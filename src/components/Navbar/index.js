8bfa73300f4706a189e447c3181492fbb6223b4f
who-sane <103615588@student.swin.edu.au>
2022-10-13 18:44:33 +1100

    Installed Styled Components, Created and added basic styled components for Navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from "./NavbarElements";
+import { FaBars } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">Eternal Dreams</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about"> About </NavLinks>
+            </NavItem>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
